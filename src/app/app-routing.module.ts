8dffc12e811989dc0b926a965523ec57a28ee0ad
Izabela Leme <izabela.leme@plasutil.com.br>
2023-08-07 12:54:44 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: '/home', pathMatch: 'full'},
+  {path:'home', redirectTo: HomeComponent},
+  {path:'cadastro', redirectTo: CadastroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
